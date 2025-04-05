64fdfc8c4b17f13b7380b083a4343ec5784e06ca
ParasY1724 <parasyerunkar174@gmail.com>
2025-04-05 10:09:18 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+export default function App() {
+  return (
+    <>
+      <div className='bg-red-500'>App</div>
+      <Button variant="contained">Contained</Button>
+    </>
+  )
+}
