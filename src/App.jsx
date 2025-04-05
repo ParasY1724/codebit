7dd08f6c7249b868feded7d0cdbea6327e50a8dc
piyush960 <piyushdahake096@gmail.com>
2025-04-05 15:07:26 +0530

    add other

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Button } from '@mui/material'
 import { Routes, Route } from 'react-router-dom'
 import Blogs from './components/Blogs'
 import Destinations from './components/Destinations'
+import TravelComparison from './components/TravelCompare'
+import PricingSection from './components/PricingComponent'
 
 export default function App() {
   return (
@@ -11,8 +13,8 @@ export default function App() {
         <Route path='/' element={<>hello</>} />
         <Route path='/blogs' element={<Blogs />}/>
         <Route path='/explore' element={<Destinations />}/>
-        <Route path='/compare' element={<Destinations />}/>
-        <Route path='/pricing' element={<Destinations />}/>
+        <Route path='/compare' element={<TravelComparison />}/>
+        <Route path='/pricing' element={<PricingSection />}/>
       </Routes>
     </>
   )
